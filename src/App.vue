73121f6b12910d0d86cbaaa201c7bc5e11982971
cvSoldier <610496306@qq.com>
2022-03-08 16:00:11 +0800

    使用低版本antd测试

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <a-icon type="step-forward" />
+    <a-icon type="smile" theme="twoTone" />
+    <a-icon type="heart" theme="twoTone" two-tone-color="#eb2f96" />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
--- vue.config.js
+++ vue.config.js
@@ -6,9 +6,20 @@ module.exports = {
       .rule('vue')
       .use('vue-test-loader')
         .loader('vue-test-loader')
+        .tap(options => {
+          return {
+            ...options,
+            filePath: './src/icon.js'
+          }
+        })
         .end()
   },
   configureWebpack: {
+    resolve: {
+      alias: {
+        '@ant-design/icons/lib/dist$': '@/icons.js',
+      },
+    },
     resolveLoader: {
       modules: [path.join(__dirname, './src/loaders'), 'node_modules']
     },
